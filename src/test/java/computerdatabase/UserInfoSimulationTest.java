c3e954f369ceceb44a3d7477b5cd34fa566dc94b
superleo-cn <superleo.peking@gmail.com>
2023-05-23 16:29:30 +0800

    Update UserInfoSimulationTest.java

--- src/test/java/computerdatabase/UserInfoSimulationTest.java
+++ src/test/java/computerdatabase/UserInfoSimulationTest.java
@@ -23,7 +23,7 @@ public class UserInfoSimulationTest extends Simulation {
 
   private static int USER_COUNT = 999999;
 
-  private static int TEST_USER_COUNT = 5;
+  private static int TEST_USER_COUNT = 10;
 
   private static int DURATION_SECONDS = 1;
 
