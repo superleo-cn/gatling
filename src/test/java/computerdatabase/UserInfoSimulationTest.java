5b1b85f4146ffa3cc572bc20d82fa1ccaf789ee2
ilwoziji@126.com <ilwoziji@126.com>
2023-05-23 18:53:32 +0800

    增加生成token代码

--- src/test/java/computerdatabase/UserInfoSimulationTest.java
+++ src/test/java/computerdatabase/UserInfoSimulationTest.java
@@ -1,15 +1,15 @@
 package computerdatabase;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import io.gatling.javaapi.core.ScenarioBuilder;
-import io.gatling.javaapi.core.Session;
 import io.gatling.javaapi.core.Simulation;
 import io.gatling.javaapi.http.HttpProtocolBuilder;
-
-import java.util.Random;
-import java.util.function.Function;
+import java.util.*;
 
 import static io.gatling.javaapi.core.CoreDsl.*;
 import static io.gatling.javaapi.http.HttpDsl.http;
+import static io.gatling.javaapi.http.HttpDsl.status;
 
 /**
  * https://vsvpxc0lc0.feishu.cn/docx/OQfBd6UXcoWwDWx8bThcs13nnTe
@@ -20,46 +20,66 @@ import static io.gatling.javaapi.http.HttpDsl.http;
  * 3000
  */
 public class UserInfoSimulationTest extends Simulation {
+  private static int TEST_USER_COUNT = 1;
 
-  private static int USER_COUNT = 999999;
+  private static int DURATION_SECONDS = 1;
 
-  private static int TEST_USER_COUNT = 240000;
-
-  private static int DURATION_SECONDS = 120;
-
-  private static String generateUsername(){
-    Random random = new Random();
-    return String.format("tip%s",random.nextInt(USER_COUNT));
-  }
-
-  static final class Templates {
-    public static final Function<Session, String> template = session -> {
-      String data = "{ \"username\": \"" + generateUsername() + "\",\"password\": \"abc123\",\"clientId\": \"63fd83a3bfb0172d558a50b9\",\"platform\": 4}";
-      return data;
-    };
-  }
 
   private HttpProtocolBuilder httpProtocol = http
-      //.baseUrl("http://localhost:18000")
-      .baseUrl("http://internal-k8s-pumpkin-testingr-93a2da19b6-1558117887.ap-southeast-1.elb.amazonaws.com")
+      .baseUrl("http://localhost:18000")
+      //.baseUrl("http://internal-k8s-pumpkin-testingr-93a2da19b6-1558117887.ap-southeast-1.elb.amazonaws.com")
       .inferHtmlResources()
       .userAgentHeader("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36");
 
   private ScenarioBuilder scn = scenario(UserInfoSimulationTest.class.getName())
     .exec(
-            http("/login")
-              .post("/login")
-                    .header("content-type", "application/json")
-                    .body(StringBody(Templates.template))
-                    .check(jsonPath("$.data.token").saveAs("token"))
-    )
-    .exec(
             http("/userInfo")
                     .get("/userInfo")
-                    .header("token","#{token}")
+                    .header("token",getToken())
+                    .check(status().is(200))
     );
 
     {
         setUp(scn.injectOpen(rampUsers(TEST_USER_COUNT).during(DURATION_SECONDS))).protocols(httpProtocol);
     }
+
+    private static String getToken(){
+      List<String> user = randomUser();
+      return generateToke(user.get(0),user.get(1));
+    }
+    private static List<String> randomUser(){
+      Random random = new Random();
+      return USERS.get(random.nextInt(USERS.size()));
+    }
+
+    private static String generateToke(String id,String username){
+      return createToken(id,username,"","");
+    }
+
+    public static String createToken(String id, String username, String nickname, String wallet) {
+      Date iatDate = new Date();
+
+      Calendar nowTime = Calendar.getInstance();
+      nowTime.add(Calendar.DATE, 7);
+      Date expiresDate = nowTime.getTime();
+
+      Map<String, Object> map = new HashMap<>();
+      map.put("alg", "HS256");
+      map.put("typ", "JWT");
+
+      return JWT.create().withHeader(map)
+              .withClaim("id", id)
+              .withClaim("username", username)
+              .withClaim("nickname", nickname)
+              .withClaim("wallet", wallet)
+              // sign time
+              .withIssuedAt(iatDate)
+              // expire time
+              .withExpiresAt(expiresDate)
+              .sign(Algorithm.HMAC256("7195d0728629969a"));
+    }
+
+    private static List<List<String>> USERS = List.of(
+            List.of("id","username")
+    );
 }
